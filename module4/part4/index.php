f48f8b26dfc897134fed82ad61c62659b5d59b55
arifulhaque-wp <arifulhaque.wp@gmail.com>
2023-05-22 18:15:49 +0600

    part4

---
+++ module4/part4/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// define variable and operations
+
+$num1 = 123;
+$num2 = 43;
+$num3 = 2;
+
+echo "num1 = " . $num1 . "\n";
+echo "num2 = " .  $num2 . "\n";
+echo "num3 = " .  $num3 . "\n";
+
+echo "Sum of num1 & num2 = " . $num1 + $num2 . "\n";
+
+echo "Subtration of num1 & num2 = " . $num1 - $num2 . "\n";
+
+echo "Multiplication of num1 & num2 = " . $num1 * $num2 . "\n";
+
+echo "Division of num1 & num2 = " . $num1 / $num2 . "\n";
+
+echo "Modulas of num1 & num2 = " . $num1 % $num2 . "\n";
+
+echo "Exponential of num1 & num3 = " . $num1 ** $num3 . "\n";
