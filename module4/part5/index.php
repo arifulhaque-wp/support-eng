86aa9d2875454c3ac7e3a38d278b7d07964af83c
arifulhaque-wp <arifulhaque.wp@gmail.com>
2023-05-23 14:24:32 +0600

    part5

---
+++ module4/part5/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// assaignment operator
+
+$name = "Ariful";
+$name .= " Haque"; // concatination assignment operator
+
+echo $name . "\n";
+
+// comparison operator
+
+var_dump( 10 > 5 ); // greaterthan
+var_dump( 10 < 5 ); // lessthan
+var_dump( 10 >= 5 ); // greaterthan equal to
+var_dump( 10 <= 12 ); // lessthan equal to
+var_dump( 10 == 10 ); // equal equal to
+var_dump( 10 != 19 ); // not equal equal to
+var_dump( 10 === 19 ); // identical
+var_dump( 10 === 10 ); // identical
+var_dump( 10 === "10" ); // identical equal
+var_dump( 10 !== "10" ); // not identical equal
